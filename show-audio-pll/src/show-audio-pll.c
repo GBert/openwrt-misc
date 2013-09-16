980f5b6101faf07872e8029bd463366eb0f6662c
Gerhard Bertelsmann <info@gerhard-bertelsmann.de>
2013-09-16 17:38:27 +0200

    regard PLL bypass

--- show-audio-pll/src/show-audio-pll.c
+++ show-audio-pll/src/show-audio-pll.c
@@ -27,37 +27,41 @@ int main(int argc, char **argv) {
     
     mmio_unmap(&pll_io);
 
-    audio_pll_ext_div=AUDIO_PLL_CONFIG_EXT_DIV_GET(audio_pll_config);
-    audio_pll_postplldiv=AUDIO_PLL_CONFIG_POSTPLLDIV_GET(audio_pll_config);
-    audio_pll_refdiv=AUDIO_PLL_CONFIG_REFDIV_GET(audio_pll_config);
+    if (AUDIO_PLL_CONFIG_BYPASS_GET(audio_pll_config)) {
+        printf("bypassing Audio PLL\n");
+        printf("reference frequency : %3.03f MHz\n", REF_CLK/1E6);
+    } else {
+        audio_pll_ext_div=AUDIO_PLL_CONFIG_EXT_DIV_GET(audio_pll_config);
+        audio_pll_postplldiv=AUDIO_PLL_CONFIG_POSTPLLDIV_GET(audio_pll_config);
+        audio_pll_refdiv=AUDIO_PLL_CONFIG_REFDIV_GET(audio_pll_config);
 
-    audio_pll_tgt_div_frac=AUDIO_PLL_MODULATION_TGT_DIV_FRAC_GET(audio_pll_modulation); 
-    audio_pll_tgt_div_int=AUDIO_PLL_MODULATION_TGT_DIV_INT_GET(audio_pll_modulation);
-    audio_pll_mod_start=AUDIO_PLL_MODULATION_START_GET(audio_pll_modulation);
+        audio_pll_tgt_div_frac=AUDIO_PLL_MODULATION_TGT_DIV_FRAC_GET(audio_pll_modulation); 
+        audio_pll_tgt_div_int=AUDIO_PLL_MODULATION_TGT_DIV_INT_GET(audio_pll_modulation);
+        audio_pll_mod_start=AUDIO_PLL_MODULATION_START_GET(audio_pll_modulation);
 
-    printf("audio_pll_config: %08X\n", audio_pll_config);
-    printf("audio_pll_refdiv: %04X\n", audio_pll_refdiv);
-    printf("audio_pll_postplldiv: %04X\n", audio_pll_postplldiv);
-    printf("audio_pll_ext_div: %04X\n", audio_pll_ext_div);
-    printf("\n");
+        printf("audio_pll_config: %08X\n", audio_pll_config);
+        printf("audio_pll_refdiv: %04X\n", audio_pll_refdiv);
+        printf("audio_pll_postplldiv: %04X\n", audio_pll_postplldiv);
+        printf("audio_pll_ext_div: %04X\n", audio_pll_ext_div);
+        printf("\n");
 
-    printf("audio_pll_modulation: %08X\n", audio_pll_modulation);
-    printf("audio_pll_tgt_div_int: %04X\n", audio_pll_tgt_div_int);
-    printf("audio_pll_tgt_div_frac: %04X\n", audio_pll_tgt_div_frac);
-    printf("audio_pll_mod_start: %04X\n", audio_pll_mod_start);
-    printf("\n");
+        printf("audio_pll_modulation: %08X\n", audio_pll_modulation);
+        printf("audio_pll_tgt_div_int: %04X\n", audio_pll_tgt_div_int);
+        printf("audio_pll_tgt_div_frac: %04X\n", audio_pll_tgt_div_frac);
+        printf("audio_pll_mod_start: %04X\n", audio_pll_mod_start);
+        printf("\n");
 
-    /*  <PLL frequency> = <REFCLK frequency>/ REFDIV * (DIV_INT = DIV_FRAC/(2<<18-1))/2<<POSTPLLDIV
-        <MCLK Frequency> = <PLL Frequency>/EXT_DIV F_mclk = (F_REFCLK/REFDIV) * (NINT.NFRAC)/((2<<POSTPLLPWD) * (EXT_DIV))
-    */
+        /*  <PLL frequency> = <REFCLK frequency>/ REFDIV * (DIV_INT = DIV_FRAC/(2<<18-1))/2<<POSTPLLDIV
+            <MCLK Frequency> = <PLL Frequency>/EXT_DIV F_mclk = (F_REFCLK/REFDIV) * (NINT.NFRAC)/((2<<POSTPLLPWD) * (EXT_DIV))
+        */
 
-    pll_freq_multi = audio_pll_tgt_div_int + (float)(audio_pll_tgt_div_frac/(float)(2<<17));
-    vcofreq = (float)(REF_CLK / audio_pll_refdiv) * pll_freq_multi; 
-    mclk_freq = vcofreq / (float)(2<< audio_pll_postplldiv * (audio_pll_ext_div >>1) );
-    printf("reference frequency : %3.03f MHz\n", REF_CLK/1E6);
-    printf("Audio PLL multi :    %3.03f MHz\n", pll_freq_multi);
-    printf("Audio PLL frequency : %3.03f MHz\n", vcofreq/1E6);
-    printf("Audio MCLK frequency: %3.03f MHz\n", mclk_freq/1E6);
+        pll_freq_multi = audio_pll_tgt_div_int + (float)(audio_pll_tgt_div_frac/(float)(2<<17));
+        vcofreq = (float)(REF_CLK / audio_pll_refdiv) * pll_freq_multi; 
+        mclk_freq = vcofreq / (float)(2<< audio_pll_postplldiv * (audio_pll_ext_div >>1) );
+        printf("reference frequency : %3.03f MHz\n", REF_CLK/1E6);
+        printf("Audio PLL multi :    %3.03f MHz\n", pll_freq_multi);
+        printf("Audio PLL frequency : %3.03f MHz\n", vcofreq/1E6);
+        printf("Audio MCLK frequency: %3.03f MHz\n", mclk_freq/1E6);
+    }
     return 0;
-
 }
