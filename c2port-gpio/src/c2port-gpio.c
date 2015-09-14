b6d73c00be6024f04381cae4921424ee0d06ce63
Gerhard Bertelsmann <info@gerhard-bertelsmann.de>
2015-09-14 10:16:47 +0200

    refresh the driver

--- c2port-gpio/src/c2port-gpio.c
+++ c2port-gpio/src/c2port-gpio.c
@@ -1,9 +1,7 @@
 /*
  *  Silicon Labs C2 port Linux support for GPIO
  *
- *  Copyright (c) 2007 Rodolfo Giometti <giometti@linux.it>
- *  Copyright (c) 2007 Eurotech S.p.A. <info@eurotech.it>
- *  Copyright (c) 2014 Gerhard Bertelsmann <info@gerhard-bertelsmann.de>
+ *  Copyright (c) 2015 Gerhard Bertelsmann <info@gerhard-bertelsmann.de>
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 as published by
@@ -19,15 +17,13 @@
 
 #include <linux/c2port.h>
 
-static int c2ck = 1;
-static int c2d = 0;
+static int c2ck = 255;
+static int c2d = 255;
 
 module_param(c2ck, int, S_IRUSR);
 MODULE_PARM_DESC(c2ck, "C2CK pin");
 module_param(c2d, int, S_IRUSR);
 MODULE_PARM_DESC(c2d, "C2D pin");
-
-/* --- C2 port operations --------------------------------------------------- */
 
 static void gpio_c2port_access(struct c2port_device *dev, int status)
 {
@@ -66,18 +62,16 @@ static void gpio_c2port_c2ck_set(struct c2port_device *dev, int status)
 }
 
 static struct c2port_ops gpio_c2port_ops = {
-	block_size	:512,		/* bytes */
-	blocks_num	:126,		/* total flash size: 15360 bytes */
-	access		:gpio_c2port_access,
-	c2d_dir		:gpio_c2port_c2d_dir,
-	c2d_get		:gpio_c2port_c2d_get,
-	c2d_set		:gpio_c2port_c2d_set,
-	c2ck_set	:gpio_c2port_c2ck_set,
+	.block_size	= 512,		/* bytes */
+	.blocks_num	= 30,		/* total flash size: 15360 bytes */
+	.access		= gpio_c2port_access,
+	.c2d_dir	= gpio_c2port_c2d_dir,
+	.c2d_get	= gpio_c2port_c2d_get,
+	.c2d_set	= gpio_c2port_c2d_set,
+	.c2ck_set	= gpio_c2port_c2ck_set,
 };
 
 static struct c2port_device *gpio_c2port_dev;
-
-/* --- Module stuff --------------------------------------------------------- */
 
 static int __init gpio_c2port_init(void)
 {
@@ -85,23 +79,24 @@ static int __init gpio_c2port_init(void)
 
 	ret = gpio_request(c2ck, "c2port clock");
 	if (ret)
-		goto exit;
+		return ret;
 	gpio_direction_input(c2ck);
 
 	ret = gpio_request(c2d, "c2port data");
 	if (ret)
-		goto free_gpio;
+		goto free_gpio_c2ck;
 	gpio_direction_input(c2d);
 
-	gpio_c2port_dev = c2port_device_register("C8051F502", &gpio_c2port_ops, NULL);
+	gpio_c2port_dev = c2port_device_register("uc", &gpio_c2port_ops, NULL);
 	if (!gpio_c2port_dev)
-		return -ENODEV;
+		goto free_gpios;
 
 	return 0;
 
-free_gpio:
+free_gpios:
+	gpio_free(c2d);
+free_gpio_c2ck:
 	gpio_free(c2ck);
-exit:
 	return ret;
 }
 
@@ -119,7 +114,6 @@ static void __exit gpio_c2port_exit(void)
 module_init(gpio_c2port_init);
 module_exit(gpio_c2port_exit);
 
-MODULE_AUTHOR("Rodolfo Giometti <giometti@linux.it>");
 MODULE_AUTHOR("Gerhard Bertelsmann <info@gerhard-bertelsmann.de>");
 MODULE_DESCRIPTION("Silicon Labs C2 port Linux support for GPIO");
 MODULE_LICENSE("GPL");
